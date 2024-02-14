7a115d1b4aecdc00779491844bc8137dc8fbd1de
Oreazuz <Omaddams@hotmail.com>
2024-02-14 18:33:26 +0100

    Add DbContexts, packages, DI

---
+++ ChalkBoardChat.Data/Database/AuthDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChalkBoardChat.Data.Database
+{
+    public class AuthDbContext : IdentityDbContext
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options) { }
+
+    }
+}
---
+++ ChalkBoardChat.Data/Database/MessageDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ChalkBoardChat.Data.Database
+{
+    public class MessageDbContext : DbContext
+    {
+        public MessageDbContext(DbContextOptions<MessageDbContext> options) : base(options) { }
+
+    }
+}
